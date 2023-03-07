9cc03646d82fbd57b7e949dc3fde29b8289ae8cf
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-03-07 12:38:01 +0530

    ok

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ function About() {
                             <h1 className="title-font sm:text-4xl text-xl mb-4 font-medium text-gray-900">Before they sold out
                                 <br className="hidden lg:inline-block" />readymade gluten
                             </h1>
-                            <p className="mb-8 leading-relaxed text-justify fonts4">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis libero, culpa consectetur harum explicabo ab voluptates optio blanditiis itaque, et amet, pariatur enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, enim! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, minus.</p>
+                            <p className="mb-8 leading-relaxed text-justify fonts4">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis libero, culpa consectetur harum explicabo ab voluptates optio blanditiis itaque, et amet, pariatur enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, enim! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, minu.</p>
 
                         </div>
                         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-full">
