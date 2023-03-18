4161ff5954359ab30398ea1c97307be8ebc897e0
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-03-18 14:45:26 +0530

    done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ function Hero() {
                 <video className="min-w-full min-h-full absolute object-cover" src="https://firebasestorage.googleapis.com/v0/b/videos-5c17d.appspot.com/o/My%20Video%203.mp4?alt=media&token=43cde305-bb2a-4bd9-aa45-582c7d67be3b" type="video/mp4" autoPlay  muted loop />
             </div>
 
-            <div className="video-content space-y-2 mt-36 md:mt-24 md:ml-[108px]  ">
+            <div className="video-content space-y-2 mt-24 md:mt-24 md:ml-[108px]  ">
                 <div className="bg-white w-[231px] md:w-[433px] ">
                     <h1 className="md:text-6xl text-[32px] font-bold  fonts2  bgh1" style={{ transform: 'translate(0px, 0%) opacity: 1' }}>1ST NATIONAL</h1>
                 </div>
@@ -22,7 +22,7 @@ function Hero() {
                     <h1 className="md:text-6xl text-[32px] font-bold mb-5 fonts2 bgh1" style={{ transform: 'translate(0px, 0%) opacity: 1' }}>VYAPAR EXPO 2023</h1>
                 </div>
 
-                <div className=" absolute left-[354px] top-[317px]   md:left-[87.1em] md:top-[17em] bg-[#ff0000] p-2 ">
+                {/* <div className=" absolute left-[340px] top-[385px]    md:left-[87.1em] md:top-[17em] bg-[#ff0000] p-2 ">
                     <div className="icon my-3">
                         <IoLogoWhatsapp color='white' size={30} />
                     </div>
@@ -32,28 +32,39 @@ function Hero() {
                     <div className="icon my-3">
                         <AiFillInstagram color='white' size={30} />
                     </div>
-                </div>
+                </div> */}
 
                 <div className=" ">
                     <h3 className="font-bold text-xl md:text-2xl mb-5 fonts1 ">JOIN THE LEADING BRAND AND MANUFACTURER OF FOOD & AGRICLTURE</h3>
                     <h2 className=' text-lg md:text-lg mb-5 fonts1 font-bold '>10TH – 15TH JULY 2023 | IIT – BHU CAMPUS, VARANASI</h2>
                 </div>
-                <div className=' md:py-0'>
+                <div className=" flex space-x-2  ">
+                    <div className="icon">
+                        <IoLogoWhatsapp color='white' size={30} />
+                    </div>
+                    <div className="icon">
+                        <BsFacebook color='white' size={30} />
+                    </div>
+                    <div className="icon">
+                        <AiFillInstagram color='white' size={30} />
+                    </div>
+                </div>
+                <div className=' py-3 '>
                     <Link to={'/registrationfrom'}>
                         <button type="button" class="text-white bg-[#ff0000] hover:bg-purple-800font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 fonts2 ">VISITOR REGISTRATION</button>
                     </Link>
 
                 </div>
 
-                {/* <div className=" flex justify-center space-x-2 ">
+                {/* <div className=" flex space-x-2 ">
                     <div className="icon">
-                        <IoLogoWhatsapp color='#25D366' size={30} />
+                        <IoLogoWhatsapp color='white' size={30} />
                     </div>
                     <div className="icon">
-                        <BsFacebook color='#4267B2' size={30} />
+                        <BsFacebook color='white' size={30} />
                     </div>
                     <div className="icon">
-                        <AiFillInstagram color='rgb(192 32 62)' size={30} />
+                        <AiFillInstagram color='white' size={30} />
                     </div>
                 </div> */}
 
