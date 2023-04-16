dd9bc183639ce0d99ba916abec65d0be056785fd
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-04-16 22:53:35 +0530

    done

--- src/components/BhuTeam.js
+++ src/components/BhuTeam.js
@@ -51,7 +51,7 @@ function BhuTeam() {
             </div>
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84" />
+                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://bhu.ac.in/Content/Photos/Faculties/FA000366.jpeg" />
                 <div className="flex-grow">
                   <h2 className="text-gray-900 title-font font-medium">DR. ARVIND
                   </h2>
--- src/components/Team.js
+++ src/components/Team.js
@@ -59,7 +59,7 @@ function Team() {
               <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                 <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="team/WhatsApp Image 2023-04-13 at 1.33.49 PM.jpeg" />
                 <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Shail yadav</h2>
+                  <h2 className="text-gray-900 title-font font-medium">Sahil yadav</h2>
                   <p className="text-gray-500">Marketing Incharge</p>
                 </div>
               </div>
@@ -75,7 +75,7 @@ function Team() {
             </div>
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88" />
+                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="team/WhatsApp Image 2023-04-16 at 10.46.13 PM-modified.png" />
                 <div className="flex-grow">
                   <h2 className="text-gray-900 title-font font-medium">Mr. Utkarsh Keshari</h2>
                   <p className="text-gray-500">Exhibitors Management Incharge</p>
