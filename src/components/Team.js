92f3303e6ed3446ad45623d2a730055c41926713
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-04-17 21:42:22 +0530

    all done

--- src/components/Team.js
+++ src/components/Team.js
@@ -30,7 +30,7 @@ function Team() {
             </div>
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84" />
+                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="team/ajay.JPG" />
                 <div className="flex-grow">
                   <h2 className="text-gray-900 title-font font-medium">Mr. Ajay Kr. Yadav</h2>
                   <p className="text-gray-500">Discipline Secretary</p>
@@ -39,7 +39,7 @@ function Team() {
             </div>
             <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88" />
+                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="team/mohit.JPG" />
                 <div className="flex-grow">
                   <h2 className="text-gray-900 title-font font-medium">Mr. MOHIT GUPTA</h2>
                   <p className="text-gray-500">Digital Pramotion Incharge</p>
