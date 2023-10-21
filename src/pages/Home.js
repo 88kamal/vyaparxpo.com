e81b89f2e9e897a417f74ad1c87dcf3cf38afa89
Kamal Nayan Upadhyay <93386296+88kamal@users.noreply.github.com>
2023-10-21 17:13:32 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,7 +35,7 @@ function Home() {
             {/* <Highlights/> */}
             {/* <Statistics/> */}
             <Exhibitor />
-            // <BhuTeam />
+         
             <Team />
             <ScrollBtn />
             <Map />
